b142d7d1dc34d260e806e2a6ca8d5005f3b7e6ce
nicolas <mascia.nicolas10@gmail.com>
2022-08-17 19:43:44 -0300

    tarea 2

--- models/novedadesModel.js
+++ models/novedadesModel.js
@@ -13,5 +13,33 @@ async function deleteNovedadesById(id) {
     return rows;
 }
 
+async function insertarNovedad(obj) {
+    try {
+        var query = "insert into novedades set ?";
+        var rows = await pool.query(query, [obj])
+        return rows;
+    } catch (eror) {
+        console.log(error);
+        throw error;
+    }
+}
 
-module.exports = { getNovedades }
+
+async function getNovedadById(obj) {
+    var query = "selct * form novedades where id=? ";
+    var rows = await pool.query(query, [id]);
+    return rows[0];
+}
+
+async function modificarNovedadById(obj, id) {
+    try {
+        var query = "update novedades set ? where id=?";
+        var rows = await pool.query(query, [obj, id]);
+        return rows;
+    } catch (eror) {
+        throw error;
+    }
+}
+
+
+module.exports = { getNovedades, deleteNovedadesById, insertarNovedad, getNovedadById }
